3a33eb36fc49275dbb1373ecbbe35f405bba11d8
unknown <grujicjovana44@gmail.com>
2026-02-01 13:35:25 +0100

    login

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ import LoginForm from 'components/forms/LoginForm';
 import { Metadata } from 'next';
 import 'tailwindcss';
 
+
 export const metadata: Metadata = {
   title: 'Prijava | Chat App',
   description: 'Prijavite se na Chat aplikaciju',
